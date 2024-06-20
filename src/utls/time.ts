713cd88eca11b6e5a1a46fdb58a1c99a512028cf
izz520 <1547471276@qq.com>
2024-06-20 12:03:39 +0800

    feat: update time to format

--- src/utls/time.ts
+++ src/utls/time.ts
@@ -1,5 +1,11 @@
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import 'dayjs/locale/zh-cn'; // 引入中文语言包，可选
 
-export const nextTime = (second:number)=>{
-  return dayjs().add(second,'second').format("YYYY-MM-DD HH:mm:ss");
-}
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export const nextTime = (second: number) => {
+  return dayjs().add(second, 'second').tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss');
+};
